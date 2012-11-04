a6efe6d01561149940672967614f385eb16060a9
Ladislav Vondracek <ladislav.vondracek@socialbakers.com>
2012-11-04 21:08:51 +0100

    Added info about count articles in return categories.

--- model/repositories/CategoriesRepository.php
+++ model/repositories/CategoriesRepository.php
@@ -13,4 +13,20 @@ class CategoriesRepository extends BaseRepository
 
 		$this->table = 'categories';
 	}
+
+
+	public function fetchAllBy(array $where)
+	{
+		$articles = new ArticlesRepository;
+
+		$sql = '
+			SELECT [t.id], [t.name],
+				COUNT([lt.id]) AS [count_articles]
+			FROM %n AS [t]
+				LEFT JOIN %n AS [lt] ON [lt.id_' . $this->table . ']=[t.id]
+			WHERE %and
+			GROUP BY [t.id]
+		';
+		return $this->connect->fetchAll($sql, $this->table, $articles->table, $where);
+	}
 }
